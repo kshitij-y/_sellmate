94826f0c8ae2cefc3126f72c3d0a20a407a89294
Kshitij kumar <kshitijyadav2003@gmail.com>
2025-10-14 00:46:43 +0530

    forntend proxy api url

--- apps/frontend/lib/auth-client.ts
+++ apps/frontend/lib/auth-client.ts
@@ -4,6 +4,6 @@ const FE_URL = process.env.NEXT_PUBLIC_FE_URL || "http://ec2-13-61-14-231.eu-nor
 const BE_URL = process.env.NEXT_PUBLIC_BE_URL || "http://ec2-13-61-14-231.eu-north-1.compute.amazonaws.com:3002";
 
 export const authClient = createAuthClient({
-  baseURL: `${BE_URL}/api/auth/`, // the base url of your auth server
+    baseURL: `${FE_URL}/api/auth/`, // the base url of your auth server
 });
 export const { signIn, signUp, useSession, signOut } = authClient;
